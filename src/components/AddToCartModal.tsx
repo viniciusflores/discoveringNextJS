8db8603e50281373eecd5020a3033049e658db10
Vinicius Flores <viniciusflores379@gmail.com>
2021-01-24 19:03:44 -0300

    new product page

--- src/components/AddToCartModal.tsx
+++
@@ -1,3 +0,0 @@
-export default function AddToCartModal() {
-  return <div>Do really want to add this product to cart?</div>;
-}
--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,25 +1,58 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
-import dynamic from 'next/dynamic';
+import { client } from '@/lib/prismic';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
 
-const AddToCartModal = dynamic(() => import('@/components/AddToCartModal'), {
-  loading: () => <p>Loading...</p>,
-});
+import PrismicDOM from 'prismic-dom';
+import Prismic from 'prismic-javascript';
+import { Document } from 'prismic-javascript/types/documents';
 
-export default function Product() {
+interface IProductProps {
+  product: Document;
+}
+
+export default function Product({ product }: IProductProps) {
   const router = useRouter();
-  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
-  function handleToCart() {
-    setIsAddToCartModalVisible(true);
+  if (router.isFallback) {
+    return <p>loading....</p>;
   }
 
   return (
     <div>
-      <h1>{router.query.slug}</h1>
-      <button onClick={handleToCart}>Add to Cart</button>
+      <h1>{PrismicDOM.RichText.asText(product.data.title)}</h1>
 
-      {isAddToCartModalVisible && <AddToCartModal />}
+      <img src={product.data.thumbnail.url} width='300' alt='' />
+
+      <div
+        dangerouslySetInnerHTML={{
+          __html: PrismicDOM.RichText.asHtml(product.data.description),
+        }}
+      />
+
+      <p>Price: ${product.data.price}</p>
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<IProductProps> = async (
+  context,
+) => {
+  const { slug } = context.params;
+
+  const product = await client().getByUID('product', String(slug), {});
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 5,
+  };
+};
