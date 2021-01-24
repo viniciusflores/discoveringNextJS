2859a5d890cd4107dfcde9cc8e28daf3a0724519
Vinicius Flores <viniciusflores379@gmail.com>
2021-01-23 22:26:12 -0300

    client side fetching

---
+++ readme.md
@@ -0,0 +1,5 @@
+##### How to run server
+
+perform the command
+
+> npx json-server server.json -p 3333 -w
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react'
 import { Title } from '../styles/pages/Home'
 
+interface IProduct {
+  id: string
+  title: string
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data)
+      })
+    })
+  }, [])
+
   return (
     <div>
-      <Title>Hello Rocket</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProducts => {
+            return (
+              <li key={recommendedProducts.id}>
+                {recommendedProducts.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
   )
 }
