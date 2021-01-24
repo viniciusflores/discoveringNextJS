c08ba1270a688a27d4f6d8a2f48fea706890d271
Vinicius Flores <viniciusflores379@gmail.com>
2021-01-23 22:00:29 -0300

    Add styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Title } from '../styles/pages/Home'
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello Rocket</h1>
+      <Title>Hello Rocket</Title>
     </div>
   )
 }
